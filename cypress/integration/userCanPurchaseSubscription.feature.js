ccd625a2781f8cac06f77f57f5ca9bdcce3328e8
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-24 17:08:39 +0100

    Adds sad path test and assertions to tests Adds response messages for payment as application state Fix API url and attach headers

--- cypress/integration/userCanPurchaseSubscription.feature.js
+++ cypress/integration/userCanPurchaseSubscription.feature.js
@@ -1,6 +1,6 @@
-describe('Registered User Can Purchase A SSubscription', () => {
+describe('Registered user can purchase a subscription', () => {
 
-  before(() => {
+  beforeEach(() => {
     cy.server();
     cy.route({
       method: "GET",
@@ -22,16 +22,6 @@ describe('Registered User Can Purchase A SSubscription', () => {
       url: "http://localhost:3000/api/v1/auth/**",
       response: "fixture:login.json"
     });
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/subscription",
-      response: {message: "paid"},
-      status: 200
-    });
-
-  });
-
-  it('', () => {
     cy.visit('/')
     cy.get('#main-article-div').should('not.contain', 'Subscribe!')
     cy.get("#loginButton").click();
@@ -47,6 +37,16 @@ describe('Registered User Can Purchase A SSubscription', () => {
       .contains("Subscribe!")
       .click();
     cy.wait(1000);
+
+  });
+
+  it('successfully', () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/subscriptions",
+      response: {message: "paid"},
+      status: 200
+    });
     cy.get('iframe[name^="__privateStripeFrame5"]').then($iframe => {
       const $body = $iframe.contents().find("body");
       cy.wrap($body)
@@ -65,6 +65,36 @@ describe('Registered User Can Purchase A SSubscription', () => {
         .find('input[name="cvc"]')
         .type("223");
     });
+    cy.get('#payment > button').contains('Submit').click();
+    cy.get("#message").should("contain", "Thank you for subscribing to The Reactive Herald!");
+  });
+
+  it('unsuccessfully', () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/subscriptions",
+      response: {message: "fail"},
+      status: 400
+    });
+    cy.get('iframe[name^="__privateStripeFrame5"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cardnumber"]')
+        .type("4142424242424242", { delay: 10 });
+    });
+    cy.get('iframe[name^="__privateStripeFrame6"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="exp-date"]')
+        .type("1222");
+    });
+    cy.get('iframe[name^="__privateStripeFrame7"]').then($iframe => {
+      const $body = $iframe.contents().find("body");
+      cy.wrap($body)
+        .find('input[name="cvc"]')
+        .type("223");
+    });
     cy.get('#payment > button').contains('Submit').click()
+    cy.get("#message").should("contain", "Your card number is invalid.");
   });
 });
--- src/components/StripeForm.jsx
+++ src/components/StripeForm.jsx
@@ -1,41 +1,66 @@
-import React from 'react'
+import React from "react";
 import {
   CardNumberElement,
   CardExpiryElement,
   CardCVCElement,
   injectStripe
-} from 'react-stripe-elements'
-import { Button } from 'semantic-ui-react'
-import axios from 'axios'
+} from "react-stripe-elements";
+import { Button } from "semantic-ui-react";
+import axios from "axios";
+import { connect } from "react-redux";
 
-const StripeForm = (props) => {
+const StripeForm = props => {
 
   const payWithStripe = async (event, props) => {
-    event.preventDefault()
+    event.preventDefault();
+    let headers = JSON.parse(localStorage.getItem("J-tockAuth-Storage"));
     await props.stripe.createToken().then(async response => {
       try {
-        let paymentResponse = await axios.post('http://localhost:3000/api/subscription', {stripeToken: response.token.id})
-        if (paymentResponse.data) {
-          debugger
+        let paymentResponse = await axios.post(
+          "http://localhost:3000/api/v1/subscriptions",
+          { stripeToken: response.token.id },
+          { headers: headers }
+        );
+        if (paymentResponse.status === 200) {
+          props.changePaymentMessage(
+            "Thank you for subscribing to The Reactive Herald!"
+          );
         }
-      } catch {
-
+      } catch (error) {
+        props.changePaymentMessage(response.error.message);
       }
-    })
-  }
+    });
+  };
   return (
-    <form id="payment" onSubmit={event => payWithStripe(event, props)}>
-      <label>Card number </label>
-      <CardNumberElement />
-      <label> Expiration date</label>
-      <CardExpiryElement />
-      <label>CVC</label>
-      <CardCVCElement />
-      <Button >
-        Submit
-      </Button>
-    </form>
-  )
-}
+    <>
+      <form id="payment" onSubmit={event => payWithStripe(event, props)}>
+        <label>Card number </label>
+        <CardNumberElement />
+        <label> Expiration date</label>
+        <CardExpiryElement />
+        <label>CVC</label>
+        <CardCVCElement />
+        <Button>Submit</Button>
+      </form>
+      <p id="message">{props.paymentMessage}</p>
+    </>
+  );
+};
 
-export default injectStripe(StripeForm)
+const mapStateToProps = state => {
+  return {
+    paymentMessage: state.paymentMessage
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changePaymentMessage: message => {
+      dispatch({ type: "CHANGE_PAYMENTMESSAGE", payload: message });
+    }
+  };
+};
+
+export default injectStripe(
+  connect(mapStateToProps, mapDispatchToProps)(StripeForm)
+);
