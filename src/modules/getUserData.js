8f2f2a9b5a982632b75ff5e8583bda607b805868
mantanquzzmo <quzzmo@hotmail.com>
2020-01-24 13:13:07 +0100

    Changes then, catch to try catch to be consistent
    Co-authored-by: Manasa Rao <drmanasa18.rao@gmail.com>

--- src/modules/getUserData.js
+++ src/modules/getUserData.js
@@ -2,18 +2,14 @@ import axios from "axios";
 
 const getUserData = async id => {
   let headers = JSON.parse(localStorage.getItem("J-tockAuth-Storage"));
-  let response = await axios
-    .get(`/admin/users/${id}`,
-    {
+  try {
+    let response = await axios.get(`/admin/users/${id}`, {
       headers: headers
-    })
-    .then(response => {
-      return response.data.user;
-    })
-    .catch(error => {
-      return error.message;
     });
-    return response
+    return response.data.user;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export { getUserData };
