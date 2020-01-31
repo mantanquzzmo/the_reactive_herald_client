ba19d70e15517ad8c597b6b7dbec662fe2549a81
Eevan <eevan.redon@gmail.com>
2020-01-31 11:25:32 +0100

    add geolaction from browser

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next'
 const Weather = () => {
   let [weatherDisplay, setWeatherDisplay] = useState("")
   let [gotWeather, setGotDisplay] = useState(false)
+  let [coords, setCoords] = useState("")
   const { t } = useTranslation('common')
 
   const loadWeatherData = async () => {
@@ -17,24 +18,34 @@ const Weather = () => {
     }
   }
 
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((loc) => {
+      setCoords([loc.coords.latitude, loc.coords.longitude])
+    })
+  }
+
   useEffect(() => {
+    getLocation()
     loadWeatherData()
   }, []);
-  debugger
+  
   return (
       <>
         {gotWeather &&
         (<div className="weatherMain">
-          <p>{weatherDisplay.name}</p>
           <p>
+            {weatherDisplay.name}
+            <br />
             {t('weather.currently')}:
             {parseFloat(weatherDisplay.main.temp -273.15).toFixed(1)}&#8451;
-          </p>
-          <p>
-            {t('weather.feelsLike')}: 
+            <br />
+            {t('weather.feelsLike')}:
             {parseFloat(weatherDisplay.main.feels_like -273.15).toFixed(1)}&#8451;
+            <br />
+            {weatherDisplay.weather[0].description}
+            <br />
+            {coords[0]}
           </p>
-          <p>{weatherDisplay.weather[0].description}</p> 
         </div>)}
       </>
   );
