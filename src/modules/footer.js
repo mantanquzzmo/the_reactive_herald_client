6584fa6f8156430e6b4577c48a1162d88fad7dba
mantanquzzmo <quzzmo@hotmail.com>
2020-02-03 00:33:02 +0100

    fixes onthisday new syntax

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const getOnThisDay = async () => {
   let today = new Date();
-  let dd = String(today.getDate()).padStart(2, "0");
+  let dd = String(today.getDate()).padStart(1);
   let mm = String(today.getMonth() + 1).padStart(1);
+  debugger
 
   try {
     let response = await axios.get(
